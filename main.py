eb3d9407c26152ce96958443f4700a0535ec43e6
rana ayman <rana486999@gmail.com>
2022-03-29 16:22:10 +0200

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.bgcolor("lightblue")
+turtle.color('lavender')
+turtle.begin_fill()
+turtle.pensize(7)
+turtle.left(87)
+turtle.forward(133)
+turtle.circle(80, 220)
+turtle.right(140)
+turtle.circle(80, 220)
+turtle.forward(133)
+turtle.end_fill()
+turtle.done()
